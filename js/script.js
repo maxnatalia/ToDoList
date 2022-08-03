1631270af9ee786c71fb8afdb9290336d7c94563
Natalia Mazur-Zurek <nataliamazur1988@gmail.com>
2022-08-03 14:38:27 +0200

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
              ${task.content}
             </li>
             `;
@@ -23,8 +23,32 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    }
+
     const init = () => {
         render();
+
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+        addNewTask(newTaskContent);
+
+
+        });
     };
 
     init();
